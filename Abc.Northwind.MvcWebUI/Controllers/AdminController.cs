42ca1ed9ed434029dfb3db74ca1aa304ccba82a9
Ahmet <ahmetundemir@aof.anadolu.edu.tr>
2019-06-19 01:55:01 +0300

    Admin paneli ve Ürünlerin Ekleme işlemi tamamlandı.

---
+++ Abc.Northwind.MvcWebUI/Controllers/AdminController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Abc.Northwind.Business.Abstract;
+using Abc.Northwind.Entities.Concrete;
+using Abc.Northwind.MvcWebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Abc.Northwind.MvcWebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductService _productService;
+
+        public AdminController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public IActionResult Index()
+        {
+            var productListViewModel = new ProductListViewModel
+            {
+                Products = _productService.GetAll()
+            };
+            return View(productListViewModel);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _productService.Add(product);
+                TempData.Add("message", "Product was successfully added");
+            }
+            return View();
+        }
+    }
+
+
+
+
+}
